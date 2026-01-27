1a0e4e1977e2e2a895535c0dc5a35a5eecd48fe4
aatay19 <156135806+aatay19@users.noreply.github.com>
2026-01-27 17:09:50 -0400

    cambios para android

--- inventario/settings.py
+++ inventario/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = "inventario.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": 'inventario_test_db',
-        "USER": 'root',
-        "PASSWORD": 'A30205683.',
-        "PORT": '3306',
-        "HOST": 'localhost',
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
     }
 }
